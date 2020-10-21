ebf8676ee8655e4750154925255caf5d9c391869
FifiLeul <fi.leul3562@gmail.com>
2020-10-20 21:25:28 -0600

    cyclically linked list seems to work! develop a thorough tester for double and cyclically linked lists

---
+++ LinkedList/src/linkedlist/CyclicallyLinkedList.java
@@ -0,0 +1,167 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package linkedlist;
+
+import APClasses.APConsole;
+
+/**
+ *
+ * @author fifiteklemedhin
+ */
+public class CyclicallyLinkedList<E> extends DoublyLinkedList<E> 
+{
+    
+    public static void main(String[] args)
+    {
+        APConsole console = new APConsole("Cyclically Linked List Tester");
+        CyclicallyLinkedList list = new CyclicallyLinkedList();
+        
+        console.println("***********************************ADDING************************************");
+        console.println("letters");
+        list.add("43802sdlja3048");
+        list.add("hello world");
+        console.println("\t" + list);
+    
+        console.println("HEAD: "+ list.tail.next.data);
+        console.println("TAIL: "+ list.head.previous.data);
+        
+    }
+
+    //add 
+    public E add(E data)
+    {
+        if(head == null)
+        {
+            head = new Node(data, null);
+           
+            size += 1;
+            updateEnds();
+            return (E) data;
+        }
+        if(head.next == null)
+        {
+            head.next = new Node(data, null, head);//previous is the head of the list
+            tail = head.next; //since the next node is null, it is the last
+            
+            size += 1;
+            updateEnds();
+            return (E) data;
+        }
+        
+        return insert(data, size - 1);
+
+    }
+    
+    public E insert( E data, int position )
+    {
+        Node<E> currentNode = head;
+        Node<E> nextNode = null;
+        Node<E> nodeBefore = null;
+         
+        if(position > size-1)
+            return null;
+        
+        
+        //if the position is at the end of the list, find the tail node and set it's next value to a node with the data
+        if(position == size - 1)
+        {
+            while(currentNode.next != null)
+               currentNode = currentNode.next;
+            
+            //the new tail of the list
+            currentNode.next = new Node(data, null,currentNode); //sets the new tail to have the old tail be its previous
+            tail = currentNode.next;
+            
+            size += 1;
+            updateEnds();
+            return (E) data;
+        }
+        //if inserting between two nodes, go one before the desired position to maintain order
+        else
+            for(int i = 0; i < position-1 && i >= 0; i++)
+                currentNode = currentNode.next;
+        
+        //stores the nodes that are being shifted over then inserts the desired node with the variable as its next property
+        nodeBefore = currentNode;
+        nextNode = currentNode.next;
+        currentNode.next = new Node(data, nextNode, nodeBefore); //adds previous attribute
+        nextNode.previous = currentNode.next;// updating the previous attribute for the index after
+        
+        size += 1;
+        updateEnds();
+        return (E) data;
+    }
+    
+    public E remove( int position )
+    {
+        if(position < 0)
+            return null;
+        
+        if(position > size - 1)
+            return null;
+        if(position == 0)
+        {
+            head = head.next;
+            
+            if(head == null) //null does not have a previous attribute to set to null
+                tail = null;
+            else
+                head.previous = null; //cannot reference a removed node
+            
+            size -= 1;
+            updateEnds();
+            return null;
+        }
+        
+        Node<E> currentNode = head;
+        Node<E> previousNode = null;
+        
+        //goes until the node before
+        for(int i = 0; i < position - 1; i++)
+            currentNode = currentNode.next;
+        
+        previousNode = currentNode;
+        currentNode = currentNode.next;
+        previousNode.next = currentNode.next;
+        
+        if(previousNode.next != null) //if removing the not tail
+            previousNode.next.previous = previousNode; //makes it so that the node after the removed node does not point back to the removed node
+        else
+            tail = previousNode;
+        
+        size -= 1;
+        updateEnds();
+        return null;
+    }
+    
+    public void updateEnds()
+    {
+        if(head != null && tail != null)
+        {
+            head.previous = tail;
+            tail.next = head;
+        }
+        
+    }
+    
+    public String toString()
+    {
+       if(head == null)
+        return "[]";
+        
+       String list = "[";
+       Node<E> currentNode = head;
+       for(int i = 0; i < size; i++)
+       {
+           list += currentNode.data + ", ";
+           currentNode = currentNode.next;
+       }
+
+       if(list.length() == 2)
+           return list.replace(",", "") + "]";
+       return list.substring(0, list.length()-2) + "]";
+    }
+}
