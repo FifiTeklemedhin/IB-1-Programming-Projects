5d55bde9d4efd3e489b265170412feff1dff1a70
Fifi Teklemedhin <fi.leul3562@gmail.com>
2021-02-11 10:21:06 -0700

    hashtable tester

--- HashTable/src/hashtable/HashTable.java
+++ HashTable/src/hashtable/HashTable.java
@@ -22,146 +22,7 @@ public class HashTable<E> implements Collection {
     private int lazyCount;
     private Object [] table;
     private Object lazyToken;
-       
-    /*QUESTIONS
-        * add(): had to take in type object instead of E, is that ok?
-        * is size inclusive of lazy tokens? yes
-        * to remove, need to have the exact item in there
-    */
-    public static void main (String[] args)
-    {
-        APConsole console = new APConsole();
-        HashTable table = new HashTable(10);
-        
-        //******************** Basic Hashtable Properties ********************
-        
-        // Adding/removing manually: works
-        console.println("ADDING/REMOVING MANUALLY: ");
-        console.println("\t" + table.add(1));
-        console.println("\t" + table.add(2));
-        console.println("\t" + table.add(3));
-        console.println("\t" + table.remove(1));
-        console.println("\t" + table.remove(2));
-        
-        // Adding in for-loop: works so long as ending index is static (ie not size/length of table, which changes)
-        console.println("\nADDING W/ LOOP: ");
-        for(Integer i = 0; i <= 5; i ++)
-         table.add(i);
-
-        // Iterator: works 
-        console.println("\nITERATOR: ");
-        
-        Iterator<HashTable> iterator = table.iterator();
-        
-        for(Object i: table)
-            console.println("\t" + i);
-      
-        // Size/refresh/lazycount: works 
-        console.println("\nSIZE/REFRESH/LAZYCOUNT: ");
-   
-        table.refresh();
-
-        console.println("\t" + table.size());
-        console.println("\t" + table.lazyCount);
-       
-        
-        // Contains/isEmpty: works
-        console.println("\nCONTAINS/ISEMPTY: ");
-        
-        console.println("\t" + table.contains(1));
-        console.println("\t" + table.isEmpty());
-        
-        //******************** Collection Properties ********************
-        
-        //toArray(): works
-        Object[] modArr = table.toArray();
-    
-        console.println("\nTO ARRAY: ");
-        for(int i = 0 ; i < modArr.length; i ++)
-            console.println("\t" + modArr[i]);
-         
-        //TODO: toArray(Obj[])
-        console.println("\nTO ARRAY(Obj[]):");
-        
-        String[] hundredsArr = new String[10];
-            for(int i = 100; i < hundredsArr.length; i+= 100)
-                hundredsArr[i ] = i + "";
-        //table.toArray(hundredsArr);
-        console.print("\t");
-        for(Object i: table)
-            console.print(i.getClass() + ", ");
-        
-        
-        //containsAll(): works
-        console.println("\nCONTAINS ALL: ");
-        
-            //should return true
-            console.println("\t" + table.containsAll(table));
-            
-            //should return true
-            ArrayList trueArr = new ArrayList();
-            Object[] toArr = table.toArray();
-            for(Object i : toArr)
-            {
-                trueArr.add(i);
-            }
-            console.println("\t" + table.containsAll(trueArr));
-        
-            //should return false
-            ArrayList falseArr = new ArrayList();
-            falseArr.add(0);
-            falseArr.add(1);
-            falseArr.add(2);
-            falseArr.add(3);
-            falseArr.add(4);
-            falseArr.add(5);
-            falseArr.add(6);
-            console.println("\t" + table.containsAll(falseArr));
-        
-        //addAll(): works
-        console.println("\nADD ALL: ");
-        
-            //should not work
-            ArrayList tensArr = new ArrayList();
-            for(int i = 10; i < 100; i+= 10)
-                tensArr.add(i + "");
-     
-            console.println("\t" + table.addAll(tensArr));
-        
-            console.print("\t");
-            for(Object i: table.toArray())
-            console.print(i + ", ");
-        
-           //should work
-           console.println("\n");
-           tensArr = new ArrayList();
-           for(int i = 10; i < 100; i+= 10)
-                tensArr.add(i);
-
-           console.println("\t" + table.addAll(tensArr));
-
-           console.print("\t");
-           for(Object i: table.toArray())
-               console.print(i + ", ");
-
-        //removeAll(): works
-        console.println("\n\nREMOVE ALL: ");
-        table.removeAll(tensArr);
-        console.print("\t");
-        for(Object i: table.toArray())
-               console.print(i + ", ");
-                
-        //retainAll(): works
-        console.println("\nRETAIN ALL: ");
-        table.addAll(tensArr);
-        table.retainAll(tensArr);
-        console.print("\t");
-        for(Object i: table)
-               console.print(i + ", ");        
-
-        
-        
-}
+ 
     
     public HashTable(int length)
     {
@@ -226,6 +87,15 @@ public class HashTable<E> implements Collection {
        else 
        {
            for(int j = Math.abs(item.hashCode()) % this.table.length; j < this.table.length; j++) // else fill the nearest spot after the intended index
+           {
+               if(this.table[j] == null)
+               {
+                   this.table[j] = item;
+                   this.size++;
+                   return true;
+               }
+           }
+           for(int j = 0; j < Math.abs(item.hashCode()) % this.table.length; j++) // else fill the nearest spot after the intended index
            {
                if(this.table[j] == null)
                {
--- HashTable/src/hashtable/Tester.java
+++ HashTable/src/hashtable/Tester.java
@@ -5,6 +5,8 @@
  */
 package hashtable;
 
+import APClasses.APConsole;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -14,41 +16,133 @@ import java.util.Iterator;
 public class Tester {
     public static void main (String[] args)
     {
+        APConsole console = new APConsole("HashTable Tester");
         HashTable table = new HashTable(10);
         
+        //******************** Basic Hashtable Properties ********************
+        
         // Adding/removing manually: works
-        System.out.println("ADDING/REMOVING MANUALLY: ");
-        System.out.println(table.add(1));
-        System.out.println(table.add(2));
-        System.out.println(table.add(3));
-        System.out.println(table.remove(1));
-        System.out.println(table.remove(2));
+        console.println("ADDING/REMOVING MANUALLY: ");
+        console.println("\t" + table.add(1));
+        console.println("\t" + table.add(2));
+        console.println("\t" + table.add(3));
+        console.println("\t" + table.remove(1));
+        console.println("\t" + table.remove(2));
         
         // Adding in for-loop: works so long as ending index is static (ie not size/length of table, which changes)
-        System.out.println("\nADDING W/ LOOP: ");
+        console.println("\nADDING W/ LOOP: ");
         for(Integer i = 0; i <= 5; i ++)
          table.add(i);
 
         // Iterator: works 
-        System.out.println("\nITERATOR: ");
+        console.println("\nITERATOR: ");
         
         Iterator<HashTable> iterator = table.iterator();
         
         for(Object i: table)
-            System.out.println(i);
-        
-        // Size/refresh/lazycount: work 
-        System.out.println("\nSIZE/REFRESH/LAZYCOUNT: ");
-        
-        //table.refresh(); private func
-        System.out.println(table.size());
-        //System.out.println(table.lazyCount); private var
+            console.println("\t" + i);
+      
+        // Size/refresh/lazycount: works 
+        console.println("\nSIZE/REFRESH/LAZYCOUNT: ");
+            //table.refresh();
+        console.println("\t" + table.size());
+            //console.println("\t" + table.lazyCount);
+       
         
         // Contains/isEmpty: works
-        System.out.println("\nCONTAINS/ISEMPTY: ");
+        console.println("\nCONTAINS/ISEMPTY: ");
         
-        System.out.println(table.contains(1));
-        System.out.println(table.isEmpty());
+        console.println("\t" + table.contains(1));
+        console.println("\t" + table.isEmpty());
+        
+        //******************** Collection Properties ********************
+        
+        //toArray(): works
+        Object[] modArr = table.toArray();
+    
+        console.println("\nTO ARRAY: ");
+        for(int i = 0 ; i < modArr.length; i ++)
+            console.println("\t" + modArr[i]);
+         
+        //TODO: toArray(Obj[])
+        console.println("\nTO ARRAY(Obj[]):");
+        
+        String[] hundredsArr = new String[10];
+            for(int i = 100; i < hundredsArr.length; i+= 100)
+                hundredsArr[i ] = i + "";
+        //table.toArray(hundredsArr);
+        console.print("\t");
+        for(Object i: table)
+            console.print(i.getClass() + ", ");
+        
+        
+        //containsAll(): works
+        console.println("\nCONTAINS ALL: ");
+        
+            //should return true
+            console.println("\t" + table.containsAll(table));
+            
+            //should return true
+            ArrayList trueArr = new ArrayList();
+            Object[] toArr = table.toArray();
+            for(Object i : toArr)
+            {
+                trueArr.add(i);
+            }
+            console.println("\t" + table.containsAll(trueArr));
+        
+            //should return false
+            ArrayList falseArr = new ArrayList();
+            falseArr.add(0);
+            falseArr.add(1);
+            falseArr.add(2);
+            falseArr.add(3);
+            falseArr.add(4);
+            falseArr.add(5);
+            falseArr.add(6);
+            console.println("\t" + table.containsAll(falseArr));
+        
+        //addAll(): works
+        console.println("\nADD ALL: ");
+        
+            //should not work
+            ArrayList tensArr = new ArrayList();
+            for(int i = 10; i < 100; i+= 10)
+                tensArr.add(i + "");
+     
+            console.println("\t" + table.addAll(tensArr));
+        
+            console.print("\t");
+            for(Object i: table.toArray())
+            console.print(i + ", ");
+        
+           //should work
+           console.println("\n");
+           tensArr = new ArrayList();
+           for(int i = 10; i < 100; i+= 10)
+                tensArr.add(i);
+
+           console.println("\t" + table.addAll(tensArr));
+
+           console.print("\t");
+           for(Object i: table.toArray())
+               console.print(i + ", ");
+
+        //removeAll(): works
+        console.println("\n\nREMOVE ALL: ");
+        table.removeAll(tensArr);
+        console.print("\t");
+        for(Object i: table.toArray())
+               console.print(i + ", ");
+                
+        //retainAll(): works
+        console.println("\nRETAIN ALL: ");
+        table.addAll(tensArr);
+        table.retainAll(tensArr);
+        console.print("\t");
+        for(Object i: table)
+               console.print(i + ", ");        
+
         
     }
     
